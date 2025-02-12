2f32d13ab83fedc64c072139b837aab49735062e
Baris Yazici <baris.yazici@franka.de>
2025-02-12 16:11:36 +0100

    fix: linting errors

--- franka_hardware/include/franka_hardware/ros_libfranka_logger.hpp
+++ franka_hardware/include/franka_hardware/ros_libfranka_logger.hpp
@@ -1,3 +1,18 @@
+// Copyright (c) 2025 Franka Robotics GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
@@ -15,7 +30,7 @@ class RosLibfrankaLogger : public franka::LoggingSinkInterface {
   ~RosLibfrankaLogger() override = default;
 
   // Inherited via LoggingSinkInterface
-  auto getName() const -> std::string override { return "RosLibfrankaLogger"; }
+  [[nodiscard]] auto getName() const -> std::string override { return "RosLibfrankaLogger"; }
   auto logInfo(const std::string& message) -> void override;
   auto logWarn(const std::string& message) -> void override;
   auto logError(const std::string& message) -> void override;
