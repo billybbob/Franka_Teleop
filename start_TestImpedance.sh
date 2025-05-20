d9717c977732293619b18cb53e8718781e85a145
billybbob <146324001+billybbob@users.noreply.github.com>
2025-05-20 12:10:51 +0200

    Delete start_TestImpedance.sh

--- start_TestImpedance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/ros/humble/setup.bash
-
-colcon build --packages-select test_impedance
-. install/local_setup.bash 
-ros2 run test_impedance test_impedance --ros-args --params-file ./src/test_impedance/parameters.yaml
